f488c73c958bcde75efb64f488b0fc980c91c36c
Mateus Farias Costa <mateusfariasz86@gmail.com>
2023-11-05 10:29:17 -0300

    Adicionado a estrutura para execução dos testes.

--- src/main/java/com/br/praticatestesunitarios/Main.java
+++ src/main/java/com/br/praticatestesunitarios/Main.java
@@ -4,34 +4,38 @@ import com.br.praticatestesunitarios.modelo.Usuario;
 import com.br.praticatestesunitarios.modelo.dto.UsuarioDto;
 import com.br.praticatestesunitarios.repositorio.RepositorioUsuario;
 import com.br.praticatestesunitarios.servico.ServicoUsuario;
+import com.br.praticatestesunitarios.util.AtributoInvalidoExcecao;
+import com.br.praticatestesunitarios.util.ExcecaoConflitoDeDados;
+import com.br.praticatestesunitarios.util.RecursoNaoEncontradoExcecao;
 
-import java.io.Console;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static String linhaDelimitadora = "\n---------------------------------------------------------------------------------------------------------\n";
 
     public static void main(String[] args) {
         ServicoUsuario servicoUsuario = new ServicoUsuario(new RepositorioUsuario());
-        //TODO: Adicionar as dependencias para testes, e ajeitar o buffer.
         Scanner sc = new Scanner(System.in);
-
-
         boolean controle = true;
 
-        while (controle){
+        while (controle) {
+
             System.out.print("Escolha uma opção:\n1 - Salvar Usuário\n2 - Buscar Usuário Pelo Id\n3 - Listar Todos Os Usuários\n4 - Atualizar Usuário Pelo Id\n5 - Deletar Usuário Pelo Id\n6 - Encerrar o Programa\n--> ");
             int opcao = sc.nextInt();
 
             switch (opcao) {
-                case 1 -> salvarUsuario(servicoUsuario);
-                case 2 -> buscarUsuarioPeloId(servicoUsuario);
+                case 1 -> salvarUsuario(sc, servicoUsuario);
+                case 2 -> buscarUsuarioPeloId(sc, servicoUsuario);
                 case 3 -> listarTodosOsUsuarios(servicoUsuario);
-                case 4 -> atualizarUsuario(servicoUsuario);
-                case 5 -> deletarUsuario(servicoUsuario);
+                case 4 -> atualizarUsuario(sc, servicoUsuario);
+                case 5 -> deletarUsuario(sc, servicoUsuario);
                 case 6 -> controle = false;
                 default -> {
+                    System.out.println(linhaDelimitadora);
                     System.out.println("Nenhuma opção válida foi escolhida!");
+                    System.out.println(linhaDelimitadora);
+
                 }
             }
 
@@ -40,24 +44,36 @@ public class Main {
         sc.close();
     }
 
-    private static void deletarUsuario(ServicoUsuario servicoUsuario) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("\nInforme o id do usuário que você quer deletar: ");
+    private static void deletarUsuario(Scanner sc, ServicoUsuario servicoUsuario) {
+        System.out.print(linhaDelimitadora);
+        System.out.print("Informe o id do usuário que você quer deletar: ");
         Long id = sc.nextLong();
+        try {
+            servicoUsuario.deletarUsuarioPeloId(id);
+        } catch (Exception e) {
+            if (e instanceof RecursoNaoEncontradoExcecao) {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            } else {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            }
+        }
+        System.out.println("Usuário Deletado Com Sucesso!");
 
-        servicoUsuario.deletarUsuarioPeloId(id);
-        System.out.println("\nUsuário Deletado Com Sucesso!");
-
-        sc.close();
+        System.out.println(linhaDelimitadora);
     }
 
-    private static void atualizarUsuario(ServicoUsuario servicoUsuario) {
-        Scanner sc = new Scanner(System.in);
+    private static void atualizarUsuario(Scanner sc, ServicoUsuario servicoUsuario) {
+        System.out.print(linhaDelimitadora);
 
-        System.out.print("\nInforme o id do usuário que você quer buscar: ");
+        sc.nextLine();
+        System.out.print("Informe o id do usuário que você quer buscar: ");
         Long id = sc.nextLong();
-
         sc.nextLine();
 
         System.out.print("\nDigite o novo nome do usuário: ");
@@ -66,44 +82,105 @@ public class Main {
 
         System.out.print("Digite novo o CPF do usuário: ");
         String cpf = sc.nextLine();
+        Usuario usuario = new Usuario();
+        try {
+            usuario = servicoUsuario.atualizarUsuarioPeloId(id, new UsuarioDto(nome, cpf));
+        } catch (Exception e) {
+            if (e instanceof AtributoInvalidoExcecao) {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            } else if (e instanceof ExcecaoConflitoDeDados) {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            } else if (e instanceof RecursoNaoEncontradoExcecao) {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            } else {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            }
+        }
 
-        Usuario usuario = servicoUsuario.atualizarUsuarioPeloId(id, new UsuarioDto(nome, cpf));
-        System.out.println("\n" + usuario);
+        System.out.print("\n" + usuario);
 
-        sc.close();
+        System.out.print(linhaDelimitadora);
+
     }
 
     private static void listarTodosOsUsuarios(ServicoUsuario servicoUsuario) {
+        System.out.print(linhaDelimitadora);
+
         List<Usuario> usuarios = servicoUsuario.listarTodos();
 
-        System.out.println("\nUsuários Cadastrados:");
-        for (Usuario usuario : usuarios){
-            System.out.println(usuario);
+        System.out.println("Usuários Cadastrados:");
+        if (usuarios.isEmpty()) {
+            System.out.println("Nunhum Usuário Cadastrado!");
         }
+        for (Usuario usuario : usuarios) {
+            System.out.print("\n");
+            System.out.print(usuario);
+        }
+        System.out.print(linhaDelimitadora);
+
     }
 
-    private static void buscarUsuarioPeloId(ServicoUsuario servicoUsuario) {
-        Scanner sc = new Scanner(System.in);
+    private static void buscarUsuarioPeloId(Scanner sc, ServicoUsuario servicoUsuario) {
+        System.out.print(linhaDelimitadora);
 
-        System.out.print("\nInforme o id do usuário que você quer buscar: ");
+        System.out.print("Informe o id do usuário que você quer buscar: ");
         Long id = sc.nextLong();
 
         Usuario usuario = servicoUsuario.obterUmUsuarioPeloId(id);
-        System.out.println("\n"+usuario);
-        sc.close();
+        System.out.println("\n" + usuario);
+
+        System.out.print(linhaDelimitadora);
+
     }
 
-    private static void salvarUsuario(ServicoUsuario servicoUsuario) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("\nDigite o nome do usuário: ");
+    private static void salvarUsuario(Scanner sc, ServicoUsuario servicoUsuario) {
+        System.out.print(linhaDelimitadora);
+
+        sc.nextLine();
+        System.out.print("Digite o nome do usuário: ");
         String nome = sc.nextLine();
 
         System.out.print("Digite o CPF do usuário: ");
         String cpf = sc.nextLine();
+        Usuario usuario = new Usuario();
 
-        Usuario usuario = servicoUsuario.salvar(new UsuarioDto(nome, cpf));
+        try {
+            usuario = servicoUsuario.salvar(new UsuarioDto(nome, cpf));
+        } catch (Exception e) {
+            if (e instanceof AtributoInvalidoExcecao) {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            } else if (e instanceof ExcecaoConflitoDeDados) {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            } else {
+                System.out.print(linhaDelimitadora);
+                System.out.print(e.getMessage());
+                System.out.print(linhaDelimitadora);
+                return;
+            }
 
-        System.out.println("Usuário Salvo Com Sucesso!\n"+usuario);
-        sc.close();
+        }
+
+        System.out.print("\nUsuário Salvo Com Sucesso!\n" + usuario);
+
+        System.out.print(linhaDelimitadora);
+
     }
 }
---
+++ src/test/java/com/br/praticatestesunitarios/servico/ServicoUsuarioTest.java
@@ -0,0 +1,28 @@
+package com.br.praticatestesunitarios.servico;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicoUsuarioTest {
+
+    @Test
+    void salvar() {
+    }
+
+    @Test
+    void listarTodos() {
+    }
+
+    @Test
+    void obterUmUsuarioPeloId() {
+    }
+
+    @Test
+    void atualizarUsuarioPeloId() {
+    }
+
+    @Test
+    void deletarUsuarioPeloId() {
+    }
+}
